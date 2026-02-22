ab23d11fc749a3de0a3dd2c6a56026cdd4a79d12
ganatra-hari <ganatrahari100@gmail.com>
2026-02-22 19:45:46 +0530

    Refactor: Centralize API config with Vite env variables

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create a custom Axios instance that automatically uses your backend URL
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true // This is helpful since we enabled it in your backend CORS!
+    // withCredentials: true // This is helpful since we enabled it in your backend CORS!
 });
 
 export default api;
